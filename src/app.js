0acd26cce612919bfa278665b1820e15fb1ab3e8
Santosh608 <santhoshregulla@gmail.com>
2026-05-21 23:03:32 +0530

    encrypting passwords

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { validateSignupData } = require("./utils.js/validation");
+const bcrypt = require("bcrypt");
 
 // Connect to MongoDB
 connectDB();
@@ -11,13 +13,45 @@ app.use(express.json());
 //signup route
 app.post("/signup", async (req, res) => {
   try {
+    const { firstName, lastName, email, password } = req.body;
+    //validate signup data
+    validateSignupData(req);
+    //encrypt password
+    const passwordHash = await bcrypt.hash(password, 10);
+
     // Create a new user
-    const user = new User(req.body);
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
     await user.save();
 
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.error("Error creating user:", error);
+    res.status(500).json({ message: "ERROR: " + error.message });
+  }
+});
+
+//login route
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid credentials" });
+    } else {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      } else {
+        res.status(200).json({ message: "Login successful" });
+      }
+    }
+  } catch (error) {
+    console.error("Error logging in:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
